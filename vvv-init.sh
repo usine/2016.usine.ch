595ac66e69dbd550423deeeddc579d16b7743557
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-09-28 16:44:00 +0200

    add: wp-cli.yml, cleaning

--- vvv-init.sh
+++ vvv-init.sh
@@ -18,12 +18,11 @@ noroot composer install --prefer-dist
 if [ ! -f htdocs/wordpress/wp-config.php ]
 then
 	echo "Creating wp-config.php and installing WordPress"
-	cd htdocs/wordpress
 	noroot wp core config --dbname="wp_usine" --dbuser="wp_usine" --dbpass="wp_usine" --dbhost="localhost" --extra-php <<PHP
 define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content/' );
 define( 'WP_SITEURL',     'http://local.usine.dev/wordpress/');
 PHP
-	noroot wp core install --url="local.usine.dev" --title="L'Usine" --admin_user="admin" --admin_password="admin" --admin_email="mehdi.lahlou@free.fr"
+	noroot wp core install --title="L'Usine" --admin_user="admin" --admin_password="admin" --admin_email="mehdi.lahlou@free.fr"
 	noroot wp option update permalink_structure "/%year%/%monthnum%/%postname%/"
 fi
 
